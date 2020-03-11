7103786b7d874ea8f34eabf56a6619bcc48879e5
rougsig <rougsig@gmail.com>
2020-03-11 22:58:24 +0200

    Add VirtualTimeDispatcher

--- src/main/kotlin/extensions/ListExtensions.kt
+++ src/main/kotlin/extensions/ListExtensions.kt
@@ -1,10 +1,19 @@
 package extensions
 
+import component.timeline.Marble
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.merge
 
-fun <T : Any> List<T>.toFlow(): Flow<T> = flow {
-  for (element in this@toFlow) {
-    emit(element)
-  }
+@InternalCoroutinesApi
+fun <T : Any> List<Marble.Model<T>>.toTimedFlow(vtDispatcher: VirtualTimeDispatcher): Flow<Marble.Model<T>> {
+  return map {
+    flow {
+      delay(it.time.toLong())
+      emit(it.copy(time = vtDispatcher.currentTime.toDouble()))
+    }
+  }.merge()
 }
+
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,63 +3,87 @@ import component.sandbox.SandBoxOutput
 import component.timeline.Marble
 import component.timeline.Timeline
 import core.appendComponent
-import extensions.toFlow
+import extensions.VirtualTimeDispatcher
+import extensions.toTimedFlow
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.merge
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.Runnable
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
 import kotlin.browser.document
+import kotlin.browser.window
 
+@InternalCoroutinesApi
 fun main() {
-  val sandboxInput = SandBoxInput<Int>()
-  val initialState = SandBoxInput.Model<Int>(
+  val sandboxInput = SandBoxInput<Any>()
+  val initialState = SandBoxInput.Model<Any>(
     timelines = listOf(
-      Timeline.Model(listOf(
-        Marble.Model(
-          color = "red",
-          time = 20.0,
-          value = 20
-        ),
-        Marble.Model(
-          color = "red",
-          time = 40.0,
-          value = 40
-        ),
-        Marble.Model(
-          color = "red",
-          time = 60.0,
-          value = 60
+      Timeline.Model<Any>(
+        listOf(
+          Marble.Model<Any>(
+            color = "blue",
+            time = 0.0,
+            value = "A"
+          ),
+          Marble.Model<Any>(
+            color = "green",
+            time = 42.0,
+            value = "B"
+          ),
+          Marble.Model<Any>(
+            color = "orange",
+            time = 55.0,
+            value = "C"
+          )
         )
-      )),
-      Timeline.Model(listOf(
-        Marble.Model(
-          color = "yellow",
-          time = 30.0,
-          value = 30
-        ),
-        Marble.Model(
-          color = "yellow",
-          time = 50.0,
-          value = 50
-        ),
-        Marble.Model(
-          color = "yellow",
-          time = 70.0,
-          value = 70
+      ),
+      Timeline.Model(
+        listOf(
+          Marble.Model<Any>(
+            color = "yellow",
+            time = 0.0,
+            value = "1"
+          ),
+          Marble.Model<Any>(
+            color = "green",
+            time = 10.0,
+            value = "2"
+          ),
+          Marble.Model<Any>(
+            color = "orange",
+            time = 20.0,
+            value = "3"
+          )
         )
-      ))
+      )
     )
   )
   sandboxInput.setModel(initialState)
-  val sandBoxOutput = SandBoxOutput<Int>()
-  fun updateOutput(model: SandBoxInput.Model<Int>) {
+  val sandBoxOutput = SandBoxOutput<Any>()
+  fun updateOutput(model: SandBoxInput.Model<Any>) {
+    val vtDispatcher = VirtualTimeDispatcher()
     val timelines = model.timelines
-    val source0 = timelines[0].marbles.toFlow()
-    val source1 = timelines[1].marbles.toFlow()
+    val source0 = timelines[0].marbles.toTimedFlow(vtDispatcher)
+    val source1 = timelines[1].marbles.toTimedFlow(vtDispatcher)
     GlobalScope.launch {
-      sandBoxOutput.setModel(Timeline.Model(merge(source0, source1).filter { it.value > 45 }.toList()))
+      val list = source0
+        .flatMapLatest { x ->
+          source1.map {
+            it.copy(
+              time = vtDispatcher.currentTime.toDouble(),
+              value = x.value.toString() + it.value.toString())
+          }
+        }
+        .flowOn(vtDispatcher)
+        .toList()
+      sandBoxOutput.setModel(Timeline.Model(list))
     }
+    window.setTimeout({
+      println(vtDispatcher.advanceUntilIdle())
+    }, 0)
   }
   sandboxInput.setTimelinesChangeListener { model ->
     updateOutput(model)
@@ -67,4 +91,33 @@ fun main() {
   updateOutput(initialState)
   document.getElementById("app")!!.appendComponent(sandboxInput)
   document.getElementById("app")!!.appendComponent(sandBoxOutput)
+}
+
+class Queue {
+  private var elements = mutableListOf<Pair<Long, Runnable>>()
+  var time: Long = 0L
+    private set
+
+  fun post(delay: Long, block: Runnable) {
+    println("post $time $delay ${time + delay}")
+    elements.add(time + delay to block)
+  }
+
+  fun reset() {
+    elements = mutableListOf()
+    time = 0L
+  }
+
+  fun run() {
+    do {
+      elements.forEachIndexed { index, pair ->
+        val (t, b) = pair
+        if (time >= t) {
+          b.run()
+          elements.removeAt(index)
+        }
+      }
+      if (elements.all { it.first > time }) time += 1
+    } while (elements.isNotEmpty())
+  }
 }
