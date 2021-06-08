a96406a74a2c89907dd534fad765af3ec84c070c
rougsig <rougsig@gmail.com>
2021-06-08 22:37:14 +0200

    Add docs proxy
    #5

--- docs/src/main/kotlin/com/github/rougsig/kotlindocs/main.kt
+++ docs/src/main/kotlin/com/github/rougsig/kotlindocs/main.kt
@@ -1,4 +1,72 @@
 package com.github.rougsig.kotlindocs
 
-suspend fun main() {
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import java.net.URI
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+const val BASE_DOCS_URL = "https://kotlin.github.io/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines.flow"
+
+fun main() {
+  embeddedServer(Netty, port = 8000) {
+    routing {
+      get("/{page}") {
+        val page = call.parameters["page"]!!
+        val uri = URI(BASE_DOCS_URL)
+        val doc = getDocsHtml("$BASE_DOCS_URL/${page.toCamelKebabCase()}.html")
+        println("$BASE_DOCS_URL/${call.parameters["page"]}.html")
+        val pageContents = doc.selectFirst("main")
+        pageContents.select("a").forEach { link ->
+          link.attr("target", "_blank")
+          val href = link.attr("href")
+          val absoluteHref = uri.resolve(href.takeWhile { it != '#' }).toString()
+          link.attr("href", absoluteHref + href.dropWhile { it != '#' })
+        }
+        val template = Jsoup.parse(Resource.read("/template.html"))
+        val root = template.selectFirst("body")
+        root.appendChild(pageContents)
+        root.select("#${page.toLowerCase()}, .api-docs-breadcrumbs").remove()
+        call.respondText(template.html(), ContentType.Text.Html)
+      }
+    }
+  }.start(wait = true)
+}
+
+private suspend fun getDocsHtml(url: String): Document = withContext(Dispatchers.IO) {
+  suspendCoroutine { cont ->
+    try {
+      cont.resume(Jsoup.connect(url).get())
+    } catch (e: Throwable) {
+      cont.resumeWithException(e)
+    }
+  }
+}
+
+private fun String.toCamelKebabCase(): String {
+  val builder = StringBuilder()
+  this.forEach {
+    if (it.isUpperCase()) {
+      builder.append("-")
+      builder.append(it.toLowerCase())
+    } else {
+      builder.append(it)
+    }
+  }
+  return builder.toString()
+}
+
+private object Resource {
+  fun read(name: String): String {
+    return Resource::class.java.getResource(name)!!.readText()
+  }
 }
